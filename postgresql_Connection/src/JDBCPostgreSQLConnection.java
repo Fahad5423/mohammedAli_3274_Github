a2b50e5039d6b6572e192b4cead771a4ac732919
HalomachXV <b2400075@ben.edu>
2022-11-29 14:08:16 -0600

    Test Table

--- postgresql_Connection/src/JDBCPostgreSQLConnection.java
+++ postgresql_Connection/src/JDBCPostgreSQLConnection.java
@@ -1,14 +1,16 @@
 /***********************************************************
  * 
- * @author Hector Hernandez, PhD
+ * @author Mohammed Ali
  * Date: 11/16/2022 7:15pm-9:59pm [W]
  *
  **********************************************************/
 // package net.javaguides.postgresql.tutorial;
 
+import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCPostgreSQLConnection {
     // private final String url = "jdbc:postgresql://localhost/myDB";
@@ -45,6 +47,27 @@ public class JDBCPostgreSQLConnection {
      */
     public static void main(String[] args) {
         JDBCPostgreSQLConnection app = new JDBCPostgreSQLConnection();
-        app.connect();
+        Connection conn = app.connect();
+        int count = 0;
+        
+        try {
+        	Statement stat = conn.createStatement();
+        	stat.execute("DROP Table Test");
+        	stat.execute("CREATE Table Test ( Name VARCHAR(20))");
+        	for(int i = 0; i < 10; i++) {
+        		stat.execute("INSERT INTO Test VALUES('Romeo')");
+        	}
+        	
+        	ResultSet result = stat.executeQuery("SELECT * FROM Test");
+        	
+        	while(result.next()) {
+        		System.out.println(result.getString("Name"));
+        		count++;
+        	}
+        	
+        	System.out.print(count + "(rows)");
+        } catch(SQLException e) {
+        	System.out.println(e.getMessage());
+        }
     }
 }
